a1b9ea139cd249373ff267b4177b925d85eb1a83
hasanalidev37 <hasanalidev37@gmail.com>
2025-05-06 14:33:57 +0000

    chore: update deployment workflow and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/elegantsudanese1/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
